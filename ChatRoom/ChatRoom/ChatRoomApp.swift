81989353e803b47512302277b07a7c4a7780a4ff
Ashraful Islam <ashraful.meche@gmail.com>
2024-04-18 14:44:38 -0500

    adds firebase init setup

--- ChatRoom/ChatRoom/ChatRoomApp.swift
+++ ChatRoom/ChatRoom/ChatRoomApp.swift
@@ -7,10 +7,25 @@
 
 import SwiftUI
 import SwiftData
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct ChatRoomApp: App {
+    
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     @State private var userSignedIn = false
+    
     var body: some Scene {
         WindowGroup {
             if userSignedIn {
