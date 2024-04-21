8cc670372b223c285299d2aa3d7fe05d44b6fe18
Ashraful Islam <ashraful.meche@gmail.com>
2024-04-21 11:43:45 -0500

    initial firebase setup for reminder clone

--- MakeItSo-ReminderClone/MakeItSo-ReminderClone/MakeItSo_ReminderCloneApp.swift
+++ MakeItSo-ReminderClone/MakeItSo-ReminderClone/MakeItSo_ReminderCloneApp.swift
@@ -5,11 +5,24 @@
 //  Created by Ashraful Islam on 4/20/24.
 //
 
-import SwiftData
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+    }
+}
 
 @main
 struct MakeItSo_ReminderCloneApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
