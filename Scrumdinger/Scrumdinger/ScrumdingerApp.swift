872cd6c349db13c4742a9bd21c5e17aca90fce6a
Ashraful Islam <ashraful.meche@gmail.com>
2024-04-25 16:39:48 -0500

    scrumdinger: adds initial code and meeting view

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ashraful Islam on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
