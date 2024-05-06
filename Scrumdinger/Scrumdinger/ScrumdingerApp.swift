04b9a229e14b8f05abb656d975b321ff08ebdcdf
Ashraful Islam <ashraful.meche@gmail.com>
2024-05-05 21:37:53 -0500

    scrumdinger: adds error handling to show error view to user with guidance

--- Scrumdinger/Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -10,15 +10,17 @@ import SwiftUI
 @main
 struct ScrumdingerApp: App {
     @StateObject private var store = ScrumStore()
+    @State private var errorWrapper: ErrorWrapper?
 
     var body: some Scene {
         WindowGroup {
             ScrumsView(scrums: $store.scrums) {
                 Task {
                     do {
+                        print("saving scrums")
                         try await store.save(scrums: store.scrums)
                     } catch {
-                        fatalError(error.localizedDescription)
+                        errorWrapper = ErrorWrapper(error: error, guidance: "Try again later.")
                     }
                 }
             }
@@ -26,8 +28,14 @@ struct ScrumdingerApp: App {
                 do {
                     try await store.load()
                 } catch {
-                    fatalError(error.localizedDescription)
+                    errorWrapper = ErrorWrapper(
+                        error: error, guidance: "Scrumdinger will load sample data and continue.")
                 }
+            }
+            .sheet(item: $errorWrapper) {
+                store.scrums = DailyScrum.sampleData
+            } content: { wrapper in
+                ErrorView(errorWrapper: wrapper)
             }
         }
     }
